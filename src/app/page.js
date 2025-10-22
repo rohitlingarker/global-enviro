951941aebcff9506743bdf0c24199d2b6edf33ff
rohitlingarker <rohitlingerker@gmail.com>
2025-10-22 12:45:13 +0530

    added custom color pallete in global css

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,3 @@
-import Image from "next/image";
-import Navbar from "../components/Navbar";
-import Carousel from "@/components/Carousel";
-import ServicesSection from "@/components/ServiceSection";  
-import Footer from "@/components/Footer";
-import HeroCarousel from "@/components/HeroCarousel/HeroCarousel.jsx";
-import HeroSection from "@/components/HeroSection/HeroSection.jsx";
 
 export default function Home() {
   
