9f5375895b5885493435cd1c65cb92239d1fb775
rohitlingarker <rohitlingerker@gmail.com>
2025-10-22 10:31:33 +0530

    merge

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,11 @@
 import Image from "next/image";
 import Navbar from "../components/Navbar";
-<<<<<<< HEAD
 import Carousel from "@/components/Carousel";
 import ServicesSection from "@/components/ServiceSection";  
 import Footer from "@/components/Footer";
-=======
 import HeroCarousel from "@/components/HeroCarousel/HeroCarousel.jsx";
 import HeroSection from "@/components/HeroSection/HeroSection.jsx";
 
->>>>>>> 2174ea18caf5530f38084a1fce1d4bfd1dafde1d
 export default function Home() {
   const slides = [
     {
@@ -35,22 +32,11 @@ export default function Home() {
     },
   ];
   return (
-<<<<<<< HEAD
     <main className="flex min-h-screen flex-col items-center justify-between bg-gray-300">
       <Navbar />
       <Carousel />
       <ServicesSection />
       <Footer />
-=======
-    <main className="flex min-h-screen flex-col items-center justify-between bg-gray-300 ">
-      {/* <HeroCarousel slides={slides} /> */}
-      {/* <HeroSection title={"Global enviro engineering"} 
-      subtitle={"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias magni, explicabo at beatae quos officia? Tenetur sunt minus eos magni."}
-      imageSrc={"https://storage.googleapis.com/uxpilot-auth.appspot.com/3e425a459b-733ddb1f886d42169401.png"}
-
-      /> */}
-      
->>>>>>> 2174ea18caf5530f38084a1fce1d4bfd1dafde1d
     </main>
   );
 }
