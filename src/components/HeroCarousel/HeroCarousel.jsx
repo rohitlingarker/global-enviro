03b253a1e5eb812cac403cfbdb2e527832a0e74c
rohitlingarker <rohitlingerker@gmail.com>
2025-10-22 11:37:10 +0530

    changes website title and logo

--- src/components/HeroCarousel/HeroCarousel.jsx
+++ src/components/HeroCarousel/HeroCarousel.jsx
@@ -3,6 +3,34 @@ import { useState, useEffect } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { cn } from '@/lib/utils'
 
+// usage 
+// const slides = [
+//   {
+//     image:
+//       "https://storage.googleapis.com/uxpilot-auth.appspot.com/3e425a459b-733ddb1f886d42169401.png",
+//     title: "Slide 1",
+//     subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam fugit laboriosam animi molestias doloremque sapiente doloribus velit nisi necessitatibus modi.",
+//     buttonText: "Button Text 1",
+//     buttonLink: "#",
+//   },
+//   {
+//     image:
+//       "https://storage.googleapis.com/uxpilot-auth.appspot.com/3e425a459b-733ddb1f886d42169401.png",
+//     title: "Slide 2",
+//     subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam fugit laboriosam animi molestias doloremque sapiente doloribus velit nisi necessitatibus modi.",
+//     buttonText: "Button Text 2",
+//     buttonLink: "#",
+//   },
+//   {
+//     image:
+//       "https://storage.googleapis.com/uxpilot-auth.appspot.com/3e425a459b-733ddb1f886d42169401.png",
+//     title: "Slide 3",
+//     subtitle: "Subtitle 3",
+//   },
+// ];
+// 
+// <HeroCarousel slides={slides} />
+
 export default function HeroCarousel({
   slides = [],
   interval = 2000,
