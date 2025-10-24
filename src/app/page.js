762b66dc68416ae832113975e782a41caf66aedf
thejas <thejasgajula@gmail.com>
2025-10-24 14:55:24 +0530

    our client

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+"use client";
+
 import Image from "next/image";
 import Navbar from "../components/Navbar";
 import HeroCarousel from "@/components/HeroCarousel/HeroCarousel.jsx";
 import HeroSection from "@/components/HeroSection/HeroSection.jsx";
+import ClientsPage from "@/components/ClientsPage";
 
 export default function Home() {
   const slides = [
@@ -9,7 +12,8 @@ export default function Home() {
       image:
         "https://storage.googleapis.com/uxpilot-auth.appspot.com/3e425a459b-733ddb1f886d42169401.png",
       title: "Slide 1",
-      subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam fugit laboriosam animi molestias doloremque sapiente doloribus velit nisi necessitatibus modi.",
+      subtitle:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam fugit laboriosam animi molestias doloremque sapiente doloribus velit nisi necessitatibus modi.",
       buttonText: "Button Text 1",
       buttonLink: "#",
     },
@@ -17,7 +21,8 @@ export default function Home() {
       image:
         "https://storage.googleapis.com/uxpilot-auth.appspot.com/3e425a459b-733ddb1f886d42169401.png",
       title: "Slide 2",
-      subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam fugit laboriosam animi molestias doloremque sapiente doloribus velit nisi necessitatibus modi.",
+      subtitle:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam fugit laboriosam animi molestias doloremque sapiente doloribus velit nisi necessitatibus modi.",
       buttonText: "Button Text 2",
       buttonLink: "#",
     },
@@ -28,15 +33,26 @@ export default function Home() {
       subtitle: "Subtitle 3",
     },
   ];
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-gray-300 ">
-      {/* <HeroCarousel slides={slides} /> */}
-      {/* <HeroSection title={"Global enviro engineering"} 
-      subtitle={"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias magni, explicabo at beatae quos officia? Tenetur sunt minus eos magni."}
-      imageSrc={"https://storage.googleapis.com/uxpilot-auth.appspot.com/3e425a459b-733ddb1f886d42169401.png"}
 
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between bg-gray-50">
+      {/* Optional Navbar */}
+      {/* <Navbar /> */}
+
+      {/* Optional Hero Section */}
+      {/* <HeroCarousel slides={slides} /> */}
+
+      {/* Optional static hero section */}
+      {/* <HeroSection
+        title="Global Enviro Engineering"
+        subtitle="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias magni, explicabo at beatae quos officia? Tenetur sunt minus eos magni."
+        imageSrc="https://storage.googleapis.com/uxpilot-auth.appspot.com/3e425a459b-733ddb1f886d42169401.png"
       /> */}
-      
+
+      {/* 👇 Clients section appears below */}
+      <section className="w-full py-12">
+        <ClientsPage />
+      </section>
     </main>
   );
 }
