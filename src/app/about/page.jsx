68e9413e104663f2fc9fb5cb228b485f56c909f7
Sindhuu001 <sindhuredde07@example.com>
2025-10-28 16:29:46 +0530

    Overview changes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,10 @@
 "use client";
 import Overview from "@/components/about/Overview";
-
+import AboutBanner from "@/components/About/AboutBanner";
 export default function AboutPage() {
   return (
     <main>
+      <AboutBanner/>
       <Overview />
     </main>
   );
