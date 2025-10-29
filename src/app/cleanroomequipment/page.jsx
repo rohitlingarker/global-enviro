99d08b6a3285f9f9c3b26ab42800f23cf618a065
thejas <thejasgajula@gmail.com>
2025-10-29 15:45:40 +0530

    cleanroomequipment

---
+++ src/app/cleanroomequipment/page.jsx
@@ -0,0 +1,9 @@
+import CleanroomEquipment from "@/components/cleanroomequipment";
+
+export default function CleanroomEquipmentPage() {
+  return (
+    <main className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <CleanroomEquipment />
+    </main>
+  );
+}
